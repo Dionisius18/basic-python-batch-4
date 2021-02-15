0a50ffc8ce28557e41d37c4a512d0860ca14cc93
Dionisius18 <dionisius.w98@gmail.com>
2021-02-15 11:45:22 +0700

    Tugas nomor 1

---
+++ Tugas-1/soal-1.py
@@ -0,0 +1,4 @@
+name = str(input('Nama anda adalah :'))
+age = int(input('Umur anda adalah :'))
+high = float(input('Tinggi badan anda adalah:'))
+print('Nama saya {}, umur saya {} tahun dan tinggi saya {} cm'.format(name,age,high))
