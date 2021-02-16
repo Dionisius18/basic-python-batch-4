0e61c8973daa8c7e6a5354a0a358f085ac20554e
Dionisius18 <dionisius.w98@gmail.com>
2021-02-16 16:04:06 +0700

    Penambahan jawaban soal 2

--- Tugas-1/soal-2.py
+++ Tugas-1/soal-2.py
@@ -1,4 +1,5 @@
 r = int(input("jari-jari lingkaran :"))
 pi = 22/7
 luas_lingkaran = pi * (r**2)
-print("Luas lingkaran dengan jari-jari {} cm adalah {} cm\u00b2".format(r,luas_lingkaran))
+float_luas = '{:05.2f}'.format(luas_lingkaran)
+print("Luas lingkaran dengan jari-jari {} cm adalah {} cm\u00b2".format(r,float_luas))
